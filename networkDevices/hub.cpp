415036f1a932ecec7ba67a57b14690c067476127
Naqeeb <naqeeburrahman0@gmail.com>
2021-05-11 19:09:50 +0530

    Data link layer case 1 fully completed without error

--- networkDevices/hub.cpp
+++ networkDevices/hub.cpp
@@ -6,3 +6,41 @@ class Hub {
     long long int macAddress;
     vector<long long int> connections;  //contain end devices no. which are connected to it 
 };
+
+struct end_devices
+{
+    string ip_address;
+    string address;
+    string data;
+    void recieve(string a)
+    {
+        if(address==a || a=="FF:FF:FF:FF")
+        {
+            cout<<"data received at: " <<address<<endl;
+        }
+        else
+        {
+            cout<<"data rejected by: "<<address<<endl;
+        }
+    }
+};
+struct hub
+{
+    string address;
+    string ip_address;
+    end_devices ed[5];
+    void send(string add_s,string add_r,string data)
+    {
+        for(int i=0;i<5;i++)
+         {
+             if(ed[i].address!="add_s")
+             ed[i].recieve(add_r);
+         }
+    }
+    void recieve(string add_s,string add_r,string data)
+    {
+        cout<<"data received at hub send by the device: "<<add_s<<endl;
+        cout<<"broadcasting data to all devices"<<endl;
+        this->send(add_s,add_r,data);
+    }
+};
--- networkDevices/switch.cpp
+++ networkDevices/switch.cpp
@@ -14,8 +14,31 @@ class endDevices {
         deviceNo=d;
         macAddress=macAddr;
     }
+    void messageStorageReceived(string message,int src)
+    {
+        messagesReceived.insert({message, src});
+    }
+
+    void messageStorageSent(string message, int dest)
+    {
+        messagesSent.insert({message, dest});
+    }
     
-    friend void tableMapping(int,int,endDevices e[],int,Switch,string message);
+    void Show()
+    {
+        cout<<"device No: "<<deviceNo<<"\n";
+        cout<<"Mac Address: "<<macAddress<<"\n";
+        cout<<"Messages Received: \n\n";
+        cout<<"Messages         Sender Info(device no)\n";
+        for(auto i: messagesReceived)
+        cout<<i.first<<"        "<<i.second<<"\n"; 
+        cout<<"Messages Sent: \n\n";
+        cout<<"Messages         Destination Info(device no)\n";
+        for(auto i: messagesSent)
+        cout<<i.first<<"        "<<i.second<<"\n"; 
+    }
+
+    friend void tableMapping(int,int,endDevices e[],int,Switch &,string message);
 };
 
 class Switch {
@@ -27,19 +50,36 @@ class Switch {
     public:
     void initialize(string macAddr,int port,int size)
     {
-        macAddr=macAddress;
+        macAddress=macAddr;
         nofPort=port;
         mappingDone=false;
         connection=new int[size+1];
-        memset(connection,0,sizeof(connection));
+        for(int i=0;i<size+1;i++)
+        connection[i]=0;
     }
-
-    friend void tableMapping(int ,int,endDevices e[] ,int ,Switch,string message);
 
     void Connection(int p,int dev)
     {
+        if(connection[p]==0)
         connection[p]=dev;
+        else
+        cout<<"already occupied\n";
     }
+
+    void Show()
+    {
+        cout<<"Mac Address: "<<macAddress<<"\n";
+        cout<<"No of port: "<<nofPort<<"\n";
+        if(mappingDone==true)
+        {
+            cout<<"Port       Mac Address: \n";
+            for(int i=1;i<=nofPort;i++)
+            cout<<i<<"             "<<table[i]<<"\n";
+        }
+        else
+        cout<<"Mapping not done\n";
+    }
+    friend void tableMapping(int ,int,endDevices e[] ,int ,Switch &,string message);
 };
 
 vector<string> framing(string message)
@@ -57,7 +97,6 @@ vector<string> framing(string message)
 vector<string> slottedAloha(vector<string> messages)
 {
     vector<string> ans;
-    srand(time(0));
     int i=10;
     int l=0;
     unordered_set<int> s; //slot used
@@ -102,7 +141,7 @@ void stopWait(vector<string> messages)
     {
         int n=rand()%6;
         if(n==2 || n==4)
-        cout<<"Packet Lost";
+        cout<<"Packet "<<sl<<" Lost\n";
         else
         {
             if(receiverWindow!=sl)
@@ -112,7 +151,7 @@ void stopWait(vector<string> messages)
             }
             else
             {
-                cout<<"Packet Received";
+                cout<<"Packet "<<sl<<" Received\n";
                 receiverWindow++;
                 int m=rand()%3;
                 if(m==2)
@@ -130,7 +169,6 @@ void stopWait(vector<string> messages)
 //implementation of selective repeat
 void selectiveRepeat(vector<string> messages)
 {
-    vector<string> receivedMessages;
     //window size of two taken
     int sl=0,sr=1; //sender window
     int receiverWindow[5]; //receiver window
@@ -143,7 +181,7 @@ void selectiveRepeat(vector<string> messages)
         int n=rand()%5;
         if(n==q.front())
         {
-            cout<<"collision happened\n";
+            cout<<"collision happened packet "<<q.front()<<" lost\n";
             q.push(q.front());
             q.pop();
         }
@@ -152,10 +190,11 @@ void selectiveRepeat(vector<string> messages)
             if(receiverWindow[q.front()]==1 && sl==q.front())
             {
                 q.pop();
+                cout<<"ACK "<<sl<<" received\n";
                 sl++,sr++;
                 if(sr<5)
                 q.push(sr);
-                if(q.size()==1)
+                if(q.size()==1 || q.size()==0)
                 {
                     sl++,sr++;
                     if(sr<5)
@@ -164,24 +203,30 @@ void selectiveRepeat(vector<string> messages)
             }
             else
             {
-                cout<<"Packet received\n";
+                cout<<"Packet "<<q.front()<<" received\n";
                 receiverWindow[q.front()]=1;
                 int y=rand()%4;
                 if(y==1)
                 {
-                    cout<<"ACK Lost\n";
+                    cout<<"ACK "<<q.front()<<" Lost\n";
                     q.push(q.front());
                     q.pop();
                 }
                 else
                 {
-                    cout<<"ACK received\n";
+                    cout<<"ACK "<<q.front()<<" received\n";
                     if(q.front()==sl)
                     {
                         sl++;
                         sr++;
                         if(sr<5)
                         q.push(sr);
+                        if(q.size()==2 && sl!=4)
+                        {
+                            sl++,sr++;
+                            if(sr<5)
+                            q.push(sr);
+                        }  
                     }
                     q.pop();
                 } 
@@ -194,7 +239,7 @@ void selectiveRepeat(vector<string> messages)
 
 
 
-void tableMapping(int src,int dst,endDevices e[],int nport,Switch s,string message)
+void tableMapping(int src,int dst,endDevices e[],int nport,Switch &s,string message)
 {
     if(s.mappingDone==false)
     {
@@ -204,7 +249,7 @@ void tableMapping(int src,int dst,endDevices e[],int nport,Switch s,string messa
             if(i==src)
             continue;
             if(s.connection[i]!=0)
-            s.table.insert({i,e[s.connection[i]-1].macAddress});
+            s.table.insert({i,e[s.connection[i]].macAddress});
         }
        s.mappingDone=true;
     }
@@ -212,52 +257,126 @@ void tableMapping(int src,int dst,endDevices e[],int nport,Switch s,string messa
     //message divided into packets 
     messages=framing(message);
     //Slotted Aloha implemented
-    messages=slottedAloha(messages);
+    int ch;
+    cout<<"Enter choice for Demonstration of protocol 1.Slotted Aloha   2.Stop and Wait   3.Selective Repeat: ";
+    cin>>ch;
+    cout<<"\n";
+    if(ch==1)
+    {
+        messages=slottedAloha(messages);
+        string mem="";
+        for(int i=0;i<messages.size();i++)
+        mem+=messages[i];
+        if(mem.size()!=message.size())
+        {
+            e[src].messageStorageSent(message,dst);
+            e[dst].messageStorageReceived(mem,src);
+        }   
+        else
+        {
+            e[src].messageStorageSent(message,dst);
+            e[dst].messageStorageReceived(message,src);
+        }
+    }
+    else if(ch==2)
+    {
+        stopWait(messages);
+        e[src].messageStorageSent(message,dst);
+        e[dst].messageStorageReceived(message,src);
+    }
+    else if(ch==3)
+    {
+        selectiveRepeat(messages);
+        e[src].messageStorageSent(message,dst);
+        e[dst].messageStorageReceived(message,src);
+    }
+    
 }
 
 void Construction()
 {
-    int n,m;
-    cout<<"No. of switch needed and no. of port in it: ";
-    cin>>n>>m;
-    Switch s[n];
-    int y;
-    cout<<"Total no. of endDevices needed: ";
-    cin>>y;
-    endDevices e[y+1];
-    int d=0;
-    for(int i=0;i<n;i++)
+    Switch s;
+    string l;
+    cout<<"Enter MacAddress of Switch : ";
+    cin>>l;
+    int m;
+    cout<<"no. of port in it: ";
+    cin>>m;
+    endDevices e[m+1];
+
+    s.initialize(l,m,m);
+    for(int j=1;j<=m;j++)
     {
-        string l;
-        cout<<"Enter MacAddress of Switch "<<i+1<<": ";
-        cin>>l;
-        int x;
-        cout<<"No. of devices you need for connection with switch "<<i+1<<" :";
-        cin>>x;
-        s[i].initialize(l,m,x);
-        for(int j=d+1;j<=x+d;j++)
-        {
-            string mac;
-            int p;
-            cout<<"enter macAddress of device: ";
-            cin>>mac;
-            cout<<"enter empty port no to which this device will be connected: ";
-            cin>>p;
-            e[j].initialize(j,mac);
-            s[i].Connection(p,j);
-        }
-        d=x;
+        string mac;
+        int p;
+        cout<<"enter macAddress of device: ";
+        cin>>mac;
+        cout<<"enter empty port no to which this device will be connected(1-"<<m<<") : ";
+        cin>>p;
+        e[p].initialize(p,mac);
+        s.Connection(p,p);
     }
+
     int ch=1;
-    while(ch==1)
+    while(ch!=0)
     {
-        int src,dst;
-        cout<<"Enter src and dest port to send message: ";
-        cin>>src>>dst;
-        string message;
-        cout<<"Enter Message to sent (length atleast greater than 10): ";
-        cin>>message;
-        tableMapping(src,dst,e,m,s[0],message);
-        cout<<"0.to exit else to continue: ";
+        cout<<"Enter choice 1.to sent message 2.to see device details 3.to see switch details  4.broadcast and collision domain in given network 0.to exit:  ";
+        cin>>ch;
+        if(ch==1)
+        {
+            int src,dst;
+            cout<<"Enter src and dest port to send message: ";
+            cin>>src>>dst;
+            string message;
+            cout<<"Enter Message to sent (length atleast greater than 10): ";
+            cin>>message;
+            tableMapping(src,dst,e,m,s,message);
+        }
+        else if(ch==2)
+        {
+            int h=1;
+            while(h!=0)
+            {
+                cout<<"Enter device no.(i.e. port no.) to see details or 0 to go back : ";
+                cin>>h;
+                if(h!=0)
+                e[h].Show();
+            }
+        }
+        else if(ch==3)
+            s.Show();
+        else if(ch==4)
+        {
+            cout<<"Broadcast Domain: 1\n";
+            cout<<"collision Domain: "<<m<<"\n"; 
+        }
     }
+}
+
+int main()
+{
+    srand(time(0));
+    cout<<"____________________________________________________________________________________________________________________________\n\n";
+    cout<<"                                                             NETWORK SIMULATOR                                             \n";
+    cout<<"____________________________________________________________________________________________________________________________\n\n";
+    int ch=1;
+    while(ch!=0)
+    {
+        cout<<"Select type of devices to make connection: \n";
+        cout<<"1.HUB and End devices  2.Switch and End Devices   3.HUB , Switch and End Devices   0.to exit: ";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:  cout<<"RED";
+                     break;
+            case 2:  Construction();
+                     break;
+            case  3: cout<<"DARK";
+                     break;
+            case 0: break;         
+            default: cout<<"Invalid choice";
+
+        }
+    }
+    return 0;
 }
